e7668091e3269f677d8036ed59e70dac14669703
ScottAgirs <scott@ijs.to>
2020-04-29 14:31:52 -0400

    [update] header + link styles
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -6,7 +6,7 @@ import Meta from '../Meta';
 
 const StyledHeader = styled.header`
   align-items: center;
-  background-color: #ff6666;
+  background: linear-gradient(to left, rgb(184, 56, 240), rgb(255, 136, 102));
   border-color: white;
   display: flex;
   height: 64px;
@@ -27,9 +27,8 @@ const StyledHeader = styled.header`
     padding: 3.25rem 2rem 2rem 2rem;
     text-decoration: none;
     &:hover {
-      color: #1c2b6d;
-      text-shadow: 1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 10px #fff,
-        1px 1px 0 #fff;
+      color: white;
+      text-shadow: 1px 1px 0 rgba(255,255,255, 0.25), 1px 1px 0 black, 3px 3px 0 orange, -1px 1px 10px #1c2b6d, 2px 2px 1px #1c2b6d;
       font-weight: 800;
     }
   }
