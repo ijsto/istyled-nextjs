a18e93c1d8921a1ed031a0bd8254fb03dd79486a
ScottAgirs <scott@ijs.to>
2021-05-24 12:12:43 -0400

    chore: Remove links
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -48,16 +48,6 @@ const Footer = () => {
         <>
           <StyledFooter>
             <Socials />
-
-            <div className="whatisthis">
-              <a
-                href="https://ijs.to"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                iJS.to
-              </a>
-            </div>
           </StyledFooter>
           <StyledFooterButton>
             <Button onClick={() => setShow(!showFooter)}>Hide Footer</Button>
--- pages/index.js
+++ pages/index.js
@@ -9,47 +9,14 @@ const App = () => {
 
       <input placeholder="Type in something nice" type="text" />
 
-      <a href="https://ijs.to/" rel="noopener noreferrer" target="_blank">
-        <p>Learn code on iJS.to</p>
-      </a>
+      <p>Learn to code on iJS.to</p>
+      <p>Visit iJS.to</p>
 
       <ul>
-        <li>
-          <a
-            href="https://ijs.to/courses/nextjs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Next.js
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://ijs.to/courses/react"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            React
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://ijs.to/courses/graphql"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            GraphQL
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://ijs.to/courses/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            and other courses
-          </a>
-        </li>
+        <li>Next.js</li>
+        <li>React</li>
+        <li>GraphQL</li>
+        <li>and other courses</li>
       </ul>
     </div>
   );
